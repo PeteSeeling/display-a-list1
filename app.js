aca6fec6597354b4cf9162edb99cfbb9da8c55a1
Pete Seeling <peteseeling@gmail.com>
2021-12-07 18:23:45 -0500

    2nd array done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import { dogBreeds } from './array-dogBreeds.js';
 import { renderDogBreeds } from './render-dogBreeds.js';
 import { dogInfo } from './array-dogInfo.js';
+import { renderDogInfo } from './render-dogInfo.js';
 const dogBreedsEl = document.getElementById('dog-breeds-list')
 const dogInfoEl = document.getElementById('pedigree');
 ;
@@ -12,32 +13,9 @@ for (let dog of dogBreeds){
 }
 
 for (let pedigree of dogInfo){
-  
-const div = document.createElement('div');
 
-const p = document.createElement('p');
-const img = document.createElement('img');
-div.classList.add('pedigree');
-
-p.textContent = pedigree.name;
-img.src = pedigree.image;
-
-div.append(p,img)
-dogInfoEl.append(div);
-
-
-div.style.fontSize = `${pedigree.age}rem`;
-
-if (pedigree.name === 'dingo'){
-  div.style.backgroundColor= 'red';
-}
-if (pedigree.chipped === 'no'){
-  div.style.transform= 'rotate(10deg)';
-}
-  
-console.log(p)
-  //console.log(dogInfo[1].name);
-  
+  const p = renderDogInfo(pedigree);
+  dogInfoEl.append(p);
 }
 
 
---
+++ render-dogInfo.js
@@ -0,0 +1,28 @@
+
+
+ export function renderDogInfo(pedigree){
+
+const div = document.createElement('div');
+const dogInfoEl = document.getElementById('pedigree');
+const p = document.createElement('p');
+const img = document.createElement('img');
+div.classList.add('pedigree');
+
+p.textContent = pedigree.name;
+img.src = pedigree.image;
+
+div.append(p,img)
+dogInfoEl.append(div);
+
+
+div.style.fontSize = `${pedigree.age}rem`;
+
+if (pedigree.name === 'dingo'){
+  div.style.backgroundColor= 'red';
+}
+if (pedigree.chipped === 'no'){
+  div.style.transform= 'rotate(10deg)';
+}
+return div;
+  //console.log(dogInfo[1].name);
+}
