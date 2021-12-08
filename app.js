34a1ca5b28291a8f1850b01c5f7e932db839d5fb
Pete Seeling <peteseeling@gmail.com>
2021-12-07 20:51:33 -0500

    3rd array finished

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ import { renderDogBreeds } from './render-dogBreeds.js';
 import { dogInfo } from './array-dogInfo.js';
 import { renderDogInfo } from './render-dogInfo.js';
 import { animalFriends } from './array-animalFriends.js';
+import { renderAnimalFriends } from './render-animalFriends.js';
 const dogBreedsEl = document.getElementById('dog-breeds-list')
 const dogInfoEl = document.getElementById('pedigree');
+const animalFriendsEl = document.getElementById('friends');
 ;
 
 for (let dog of dogBreeds){
@@ -19,49 +21,12 @@ for (let pedigree of dogInfo){
   dogInfoEl.append(p);
 }
 
-const animalFriendsEl = document.getElementById('friends');
 for (let kitten of animalFriends){
-
-  const outerDiv = document.createElement('div');
-  const kittenNameEl = document.createElement('p');
-  const kittenAgeEl = document.createElement('p');
-  const kittenImgEl = document.createElement('img');
-
-  const friendsDiv = document.createElement('div');
-  const friendNameEl = document.createElement('p');
-  const friendTypeEl = document.createElement('p');
-  const friendImgEl = document.createElement('img');
-
- outerDiv.classList.add('outer-div');
- friendsDiv.classList.add('friend');
-
- kittenNameEl.textContent = kitten.name;
- kittenAgeEl.textContent = kitten.age;
- kittenImgEl.src = kitten.url_image;
-
- friendNameEl.textContent = kitten.friend.name;
- friendTypeEl.textContent = kitten.friend.type;
- friendImgEl.src = kitten.friend.url_image;
-
- outerDiv.append(kittenNameEl, kittenAgeEl, kittenImgEl);
- friendsDiv.append(friendNameEl, friendTypeEl, friendImgEl);
-
- animalFriendsEl.append(outerDiv, friendsDiv);
- console.log(kitten.friend.name);
-
- if (kitten.name === 'leo'){
-   outerDiv.style.backgroundColor = 'magenta';
- }
- if ( kitten.friend.type === 'large bear'){
-   friendsDiv.style.transform = 'rotate(12deg)';
- }
-
- if ( kitten.age === 9){
-   outerDiv.style.backgroundColor = 'green';
- }
- friendsDiv.style.fontSize = `${kitten.name.length}.01rem`;
-
+  const p = renderAnimalFriends(kitten);
+  animalFriendsEl.append(p);
 }
+
+  
 
 
   
