c120ba4da2312b8b74a7193579e7a54babf95a0a
Pete Seeling <peteseeling@gmail.com>
2021-12-07 20:33:16 -0500

    before rendering 3rd array

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { dogBreeds } from './array-dogBreeds.js';
 import { renderDogBreeds } from './render-dogBreeds.js';
 import { dogInfo } from './array-dogInfo.js';
 import { renderDogInfo } from './render-dogInfo.js';
+import { animalFriends } from './array-animalFriends.js';
 const dogBreedsEl = document.getElementById('dog-breeds-list')
 const dogInfoEl = document.getElementById('pedigree');
 ;
@@ -16,6 +17,50 @@ for (let pedigree of dogInfo){
 
   const p = renderDogInfo(pedigree);
   dogInfoEl.append(p);
+}
+
+const animalFriendsEl = document.getElementById('friends');
+for (let kitten of animalFriends){
+
+  const outerDiv = document.createElement('div');
+  const kittenNameEl = document.createElement('p');
+  const kittenAgeEl = document.createElement('p');
+  const kittenImgEl = document.createElement('img');
+
+  const friendsDiv = document.createElement('div');
+  const friendNameEl = document.createElement('p');
+  const friendTypeEl = document.createElement('p');
+  const friendImgEl = document.createElement('img');
+
+ outerDiv.classList.add('outer-div');
+ friendsDiv.classList.add('friend');
+
+ kittenNameEl.textContent = kitten.name;
+ kittenAgeEl.textContent = kitten.age;
+ kittenImgEl.src = kitten.url_image;
+
+ friendNameEl.textContent = kitten.friend.name;
+ friendTypeEl.textContent = kitten.friend.type;
+ friendImgEl.src = kitten.friend.url_image;
+
+ outerDiv.append(kittenNameEl, kittenAgeEl, kittenImgEl);
+ friendsDiv.append(friendNameEl, friendTypeEl, friendImgEl);
+
+ animalFriendsEl.append(outerDiv, friendsDiv);
+ console.log(kitten.friend.name);
+
+ if (kitten.name === 'leo'){
+   outerDiv.style.backgroundColor = 'magenta';
+ }
+ if ( kitten.friend.type === 'large bear'){
+   friendsDiv.style.transform = 'rotate(12deg)';
+ }
+
+ if ( kitten.age === 9){
+   outerDiv.style.backgroundColor = 'green';
+ }
+ friendsDiv.style.fontSize = `${kitten.name.length}.01rem`;
+
 }
 
 
